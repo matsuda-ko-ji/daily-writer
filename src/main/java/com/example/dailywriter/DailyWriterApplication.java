8bdb2422362f69c018063af6b73fc1a83191e2d5
Koji Matsuda <fullfree764@gmail.com>
2026-09-19 18:43:02 +0900

    Initial Daily Writer project

---
+++ src/main/java/com/example/dailywriter/DailyWriterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailywriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dailywriter/DailyWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailywriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
