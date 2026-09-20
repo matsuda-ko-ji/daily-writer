209b91296bb56931e4b1226726918ce6900dde0d
Koji Matsuda <fullfree764@gmail.com>
2026-09-20 17:55:48 +0900

    Connect message service to home page

--- src/main/java/com/example/dailywriter/controller/HomeController.java
+++ src/main/java/com/example/dailywriter/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.example.dailywriter.controller;
 
+import com.example.dailywriter.service.MessageService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final MessageService messageService;
+
+    public HomeController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        String message = messageService.generateStartMessage();
+
+        model.addAttribute("message", message);
+
         return "index";
     }
 }
